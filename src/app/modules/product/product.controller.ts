2b98d0b73859d543894f8052f91808a570a9e065
fai-sas <craftedbyfaisal@gmail.com>
2024-05-20 21:24:33 +0600

    get all products feature added

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -24,6 +24,25 @@ const createProduct = async (req: Request, res: Response) => {
   }
 }
 
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductsFromDB()
+
+    res.status(status.OK).json({
+      totalResult: result.length,
+      success: true,
+      message: 'products retrieved successfully',
+      data: result,
+    })
+  } catch (error: any) {
+    res.status(status.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: error.message,
+    })
+  }
+}
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 }
