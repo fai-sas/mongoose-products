c850f085784540bcfe35f7dfddd1005d9986772a
fai-sas <craftedbyfaisal@gmail.com>
2024-05-22 19:30:51 +0600

    route not found middleware added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
 import { ProductRoutes } from './app/modules/product/product.route'
 import { OrderRoutes } from './app/modules/order/order.route'
+import notFound from './app/middlewares/notFound'
+
 const app: Application = express()
 
 //parsers
@@ -15,5 +17,7 @@ app.use('/api/orders', OrderRoutes)
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+app.use(notFound)
 
 export default app
